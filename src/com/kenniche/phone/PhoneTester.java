9f8518854f597bad0411cb533108d3a0c3d51f01
Kenniche <kenniche23@gmail.com>
2021-10-31 15:04:04 -0600

    add some changes

--- src/com/kenniche/phone/PhoneTester.java
+++ src/com/kenniche/phone/PhoneTester.java
@@ -2,12 +2,12 @@ package com.kenniche.phone;
 
 public class PhoneTester {
     public static void main(String[] args) {
-        Galaxy s9 = new Galaxy("S9", 99, "T-Mobile","Cuack Cuack");
-        IPhone iPhone10 = new IPhone("X", 100, "ATT", "Run Run");
-        s9.displayInfo();
-        System.out.println("Unlocked Method: " + s9.unlock());
-        iPhone10.displayInfo();
-        System.out.println("Unlocked Method: " + iPhone10.unlock());
+        Galaxy s21 = new Galaxy("S21", 80, "Claro","Cuack Cuack");
+        IPhone iPhone13 = new IPhone("XIII", 95, "MoviStar", "Run Run");
+        s21.displayInfo();
+        System.out.println("Unlocked Method: " + s21.unlock());
+        iPhone13.displayInfo();
+        System.out.println("Unlocked Method: " + iPhone13.unlock());
     
         // Can not create the class Phone because it is abstract class				
         //Phone myPhone = new Phone();
